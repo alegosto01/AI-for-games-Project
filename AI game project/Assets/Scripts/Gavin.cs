1544daf1b99714c1335dc11b105bea8fa7a7be79
alegosto01 <ale.gostoli@gmail.com>
2023-10-15 20:50:09 +0100

    Initial commit

---
+++ AI game project/Assets/Scripts/Gavin.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gavin : MonoBehaviour
+{
+    public Transform transform;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += new Vector3(0,0,4) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += new Vector3(0,0,-4) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(4,0,0) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-4,0,0) * Time.deltaTime;
+        }
+        if(transform.position.x > 5) {
+            Debug.Log("You won!");
+        }
+        
+    }
+}
---
+++ AI game project/Assets/Scripts/PatrolState.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolState : State
+{
+    // public bool patrolState;
+    public Transform[] points = new Transform[2];
+    private int destPoint = 0;
+    public UnityEngine.AI.NavMeshAgent agent;
+
+    public override State RunCurrentState() {
+        if (!agent.pathPending && agent.remainingDistance < 0.5f) {
+            GotoNextPoint();
+        }
+        return this;
+    }
+
+
+    void GotoNextPoint() {
+        if (points.Length == 0)
+            return;
+
+        agent.destination = points[destPoint].position;
+
+        destPoint = (destPoint + 1) % points.Length;
+    }
+
+    void Update () {
+        // Choose the next destination point when the agent gets
+        // close to the current one.
+    }
+
+    // void Start(){
+    //     points[0].position = new Vector3(3.33999991f,0.360000014f,-2.58999991f);
+    //     points[1].position = new Vector3(3.33999991f,0.360000014f,2.58999991f);
+    // }
+}
