f9eca59bb7dabd549b20569e4c47aa7207bfe65f
alegosto01 <ale.gostoli@gmail.com>
2023-12-11 16:01:29 +0000

    Removed some useless script and added the old explore state

--- AI game project/Assets/Scripts/Gavin/OLD_EXPLORE_STATE.cs
+++ AI game project/Assets/Scripts/Gavin/OLD_EXPLORE_STATE.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RunAwayState : MonoBehaviour
+public class OLD_EXPLORE_STATE : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
